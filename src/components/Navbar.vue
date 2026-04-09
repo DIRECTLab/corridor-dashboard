b055ef3cd183d74a74299e44145604a9dcf02887
Clayton Montgomery <claymtgy@gmail.com>
2026-04-09 14:35:49 -0600

    Adds logo to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,9 @@
   <v-app-bar color="primary" density="compact" elevation="2" :fixed="false" :absolute="false">
     <v-app-bar-title class="d-flex align-center">
       <router-link to="/" class="d-flex align-center text-decoration-none" style="color: inherit;">
-        <span class="aspire-wordmark mr-2">ASPIRE</span>
+        <span class="navbar-logo-circle mr-2">
+          <img :src="logoUrl" alt="ASPIRE" class="navbar-logo" />
+        </span>
         <span class="text-h6">Heavy Duty Freight Electrification Explorer</span>
       </router-link>
     </v-app-bar-title>
@@ -14,6 +16,7 @@
 </template>
 
 <script setup>
+import logoUrl from '../logo/logo.svg?url'
 </script>
 
 <style scoped>
@@ -21,10 +24,23 @@
   min-height: 48px !important;
 }
 
-.aspire-wordmark {
-  color: #fff;
-  font-weight: 700;
-  letter-spacing: 0.08em;
-  font-size: 0.9rem;
+.navbar-logo-circle {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #fff;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.12);
+}
+
+.navbar-logo {
+  display: block;
+  height: 24px;
+  width: auto;
+  max-width: 32px;
+  object-fit: contain;
 }
 </style>
